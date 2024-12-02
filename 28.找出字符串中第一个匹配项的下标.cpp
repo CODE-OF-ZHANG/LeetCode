ceffa70819f80ac7e78d8ac0ce9e8501be462fc9
CODE_OF_ZHANG <3363967646@qq.com>
2024-12-02 23:46:20 +0800

    找出字符串中第一个匹配项的下标

---
+++ 28.找出字符串中第一个匹配项的下标.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        int n = haystack.size(), m = needle.size();
+        for (int i = 0; i + m <= n; i++)
+        {
+            bool flag = true;
+            for (int j = 0; j < m; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
