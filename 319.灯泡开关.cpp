a11d302c7a85688adcaca74fbbfa2dbc689f2a9a
CODE_OF_ZHANG <3363967646@qq.com>
2025-01-07 19:56:25 +0800

    数学_灯泡开关

---
+++ 319.灯泡开关.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * [319] 灯泡开关
+ */
+
+// @lc code=start
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+// @lc code=end
+
