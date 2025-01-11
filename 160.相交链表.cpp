b12cabb13f071da1d192cdfd9bc228a22d30f9ee
CODE_OF_ZHANG <3363967646@qq.com>
2025-01-11 21:14:42 +0800

    数据结构_相交链表

---
+++ 160.相交链表.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        if (headA == nullptr || headB == nullptr)
+            return nullptr;
+        // 双指针
+        ListNode *pA = headA, *pB = headB;
+        while (pA != pB)
+        {
+            pA = pA == nullptr ? headB : pA->next;
+            pB = pB == nullptr ? headA : pB->next;
+        }
+        return pA;
+    }
+};
+// @lc code=end
