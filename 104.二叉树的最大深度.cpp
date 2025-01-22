12c6fb57e06674771f07495435073036d7ec82c1
CODE_OF_ZHANG <3363967646@qq.com>
2025-01-22 21:37:37 +0800

    数据结构_二叉树的最大深度

---
+++ 104.二叉树的最大深度.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        // 空树
+        if (root == nullptr)
+            return 0;
+        // 深度优先搜索
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+// @lc code=end
