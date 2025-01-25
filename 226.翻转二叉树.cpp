904bfaa10ac562119000c346971c5c4eb51ded06
CODE_OF_ZHANG <3363967646@qq.com>
2025-01-25 21:41:37 +0800

    数据结构_翻转二叉树

---
+++ 226.翻转二叉树.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        // 空树
+        if (root == nullptr)
+            return nullptr;
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
+// @lc code=end
