2bab0bed18609bb3f86133cf730e4332e33a176d
CODE_OF_ZHANG <3363967646@qq.com>
2025-02-26 22:24:45 +0800

    平衡二叉树

---
+++ 110.平衡二叉树.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int height(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        else
+            return max(height(root->left), height(root->right)) + 1;
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        if (root == nullptr)
+            return true;
+        else
+            return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+// @lc code=end
