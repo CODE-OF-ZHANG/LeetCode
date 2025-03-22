c8e8262ed32fd4ef8d5fa55fd3c2da05b0ecb4f2
CODE_OF_ZHANG <3363967646@qq.com>
2025-03-22 23:26:39 +0800

    二叉树的所有路径

---
+++ 257.二叉树的所有路径.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=cpp
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void construct_paths(TreeNode *root, string path, vector<string> &paths)
+    {
+        if (root != nullptr)
+        {
+            path += to_string(root->val);
+            if (root->left == nullptr && root->right == nullptr)
+                paths.push_back(path);
+            else
+            {
+                path += "->";
+                construct_paths(root->left, path, paths);
+                construct_paths(root->right, path, paths);
+            }
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> paths;
+        construct_paths(root, "", paths);
+        return paths;
+    }
+};
+// @lc code=end
